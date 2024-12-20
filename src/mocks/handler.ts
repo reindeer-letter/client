0e861d2996e8de65bd025027bd4f3f582f4eb6e7
codefug <robot9917@naver.com>
2024-12-21 03:23:06 +0900

    feat: home mock server 코드 작성

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,6 +1,11 @@
+import homeHandler from "./home";
 import letterToMeHandler from "./myPage/letterToMe";
 import uncompletedLetterHandler from "./myPage/uncompletedLetter";
 
-const handler = [...uncompletedLetterHandler, ...letterToMeHandler];
+const handler = [
+  ...uncompletedLetterHandler,
+  ...letterToMeHandler,
+  ...homeHandler,
+];
 
 export default handler;
