1ed5e5ce07de0b1571d5f1505b23e9ccc689ec11
codefug <robot9917@naver.com>
2024-12-20 10:29:15 +0900

    fix: next.config.ts s3 주소 허용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "letter-bucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
