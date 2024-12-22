ccf4e54476ca48dc2a0ac1de084cbdae09aeb326
honi31 <winner020531@naver.com>
2024-12-22 18:04:39 +0900

    feat: middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -5,18 +5,12 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = request.cookies.get("token")?.value; // 엑세스 토큰 저장 이름 확인 필요!!!!
 
-  const restrictedForNotLoggedIn = ["/mypage", "/home"];
-  if (
-    !token &&
-    restrictedForNotLoggedIn.some((route) => pathname.startsWith(route))
-  )
+  const protectedRoutes = ["/mypage", "/home"];
+  if (!token && protectedRoutes.some((route) => pathname.startsWith(route)))
     return NextResponse.redirect(new URL("/login", request.url));
 
-  const restrictedForLoggedIn = ["/login", "/signUp", "/profile"];
-  if (
-    token &&
-    restrictedForLoggedIn.some((route) => pathname.startsWith(route))
-  )
+  const publicRoutes = ["/login", "/signUp", "/profile"];
+  if (token && publicRoutes.some((route) => pathname.startsWith(route)))
     return NextResponse.redirect(new URL("/home", request.url));
 
   return NextResponse.next();
