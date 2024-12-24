b7c203dc676670ed7e33013c72a07fe954b03772
codefug <robot9917@naver.com>
2024-12-24 22:07:13 +0900

    fix: 유저 데이터 관련 로직 에러 해결

--- src/providers/userDataProvider.tsx
+++ src/providers/userDataProvider.tsx
@@ -3,6 +3,7 @@
 import useGetFetch from "@/hooks/useGetFetch";
 import { GetAuthProfileResponse } from "@/types/profile";
 import { ReactNode, useEffect } from "react";
+import { isAxiosError } from "axios";
 import { useUserStore } from "./userStoreProvider";
 
 interface UserDataProviderProps {
@@ -10,9 +11,10 @@ interface UserDataProviderProps {
 }
 
 export default function UserDataProvider({ children }: UserDataProviderProps) {
-  const { data, isLoading } = useGetFetch<GetAuthProfileResponse>({
-    route: "/auth/profile",
-  });
+  const { data, isLoading, error, isError } =
+    useGetFetch<GetAuthProfileResponse>({
+      route: "/auth/profile",
+    });
   const login = useUserStore((store) => store.login);
   const setLoading = useUserStore((store) => store.setLoading);
   const logout = useUserStore((store) => store.logout);
@@ -21,9 +23,12 @@ export default function UserDataProvider({ children }: UserDataProviderProps) {
     if (isLoading) setLoading(true);
     else setLoading(false);
 
+    if (isError && isAxiosError(error) && error.response?.status === 401)
+      logout();
+
     if (!isLoading && data)
       login(data.email, data.id, data.nickName, data.profileImageUrl);
-  }, [isLoading, data, login, setLoading, logout]);
+  }, [isLoading, data, login, setLoading, logout, isError, error]);
 
   return <>{children}</>;
 }
