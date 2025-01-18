d404f203a909d2c9c68ee927175c6a596e3fa107
codefug <86544979+codefug@users.noreply.github.com>
2025-01-18 17:36:54 +0900

    refactor: s3 배포 주소 변경 설정

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "letter-bucket.s3.ap-northeast-2.amazonaws.com",
+        hostname: "reindeer-letter-bucket.s3.ap-northeast-2.amazonaws.com",
         port: "",
         pathname: "/images/**",
       },
